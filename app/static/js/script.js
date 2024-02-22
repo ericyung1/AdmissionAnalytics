edeb38b3fab8b8368d1d31a9601bb6c48678a491
Eric Yung <eyung@wustl.edu>
2024-02-22 02:17:50 -0600

    styllistic changes

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -6,3 +6,23 @@ function toggleInput(id) {
         input.style.display = "none";
     }
 }
+
+document.querySelectorAll('.form__field').forEach(function(input) {
+    input.addEventListener('focus', function() {
+        var label = this.parentNode.parentNode.querySelector('label');
+        if (label) {
+            label.style.color = 'transparent';
+            label.style.backgroundImage = 'linear-gradient(to right, #11998e, #38ef7d)';
+            label.style.webkitBackgroundClip = 'text';
+            label.style.fontWeight = '700'; /* Make the text bold */
+        }
+    });
+    input.addEventListener('blur', function() {
+        var label = this.parentNode.parentNode.querySelector('label');
+        if (label) {
+            label.style.color = '#fff';
+            label.style.backgroundImage = 'none';
+            label.style.fontWeight = 'normal'; /* Make the text normal weight */
+        }
+    });
+});
